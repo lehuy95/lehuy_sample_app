ab4c8e83937f789146306a55a34fa1c825206f7c
lehuy95 <huylevp95@gmail.com>
2020-08-06 13:41:20 +0700

    chapter 8 Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	
+  get 'sessions/new'
 root 'static_pages#home'
 
 get '/help', to: 'static_pages#help'
@@ -8,6 +9,9 @@ get '/contact', to: 'static_pages#contact'
 
 get '/signup', to: 'users#new'
 
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
 
 resources :users
 end
