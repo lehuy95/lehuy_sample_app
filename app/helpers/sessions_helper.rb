fb8227fc03648633e1ce7d6f2f86a6dbcedd4aea
lehuy95 <huylevp95@gmail.com>
2020-08-07 15:29:44 +0700

    Finish chapter10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,12 @@ module SessionsHelper
 			end
 		end
 	end
+
+	# Returns true if the given user is the current user.
+	def current_user?(user)
+		user && user == current_user
+	end
+
 	# Returns the current logged-in user (if any).
 	def current_user
 		if session[:user_id]
@@ -40,9 +46,20 @@ module SessionsHelper
 
 	# Forgets a persistent session.
 	def forget(user)
-	user.forget
-	cookies.delete(:user_id)
-	cookies.delete(:remember_token)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
+	# Redirects to stored location (or to the default).
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+	
+	# Stores the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
 	end
 
 end
